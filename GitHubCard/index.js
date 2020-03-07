6dc840f24a17cacebdced1d2e8ea83bfc4740bfc
Mona B <web25Lucius@gmail.com>
2020-03-07 07:45:38 -0700

    testing board, created elements, assigned classes, appended to board

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -9,6 +9,28 @@
 
 //    Skip to Step 3.
 
+
+/* Step 3: Create a function that accepts a single object as its only argument,
+          Using DOM methods and properties, create a component that will return the following DOM element:
+
+
+ *<div class="card">
+*  <img src={image url of user} />
+*  <div class="card-info">
+*    <h3 class="name">{users name}</h3>
+*    <p class="username">{users user name}</p>
+*    <p>Location: {users location}</p>
+*    <p>Profile:  
+*      <a href={address to users github page}>{address to users github page}</a>
+*   </p>
+*   <p>Followers: {users followers count}</p>
+*    <p>Following: {users following count}</p>
+*    <p>Bio: {users bio}</p>
+  </div>
+</div>
+
+*/
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -48,10 +70,59 @@ axios.get("https://api.github.com/users/web25Lucius")
 
 
 
+  const divCard = document.createElement('div'), 
+        imgHeroNoClass = document.createElement('img'), 
+        divCardInfo = document.createElement('div'),
+        h3Name = document.createElement('h3'), 
+        pUserName = document.createElement('p'),
+        pLocationNoClass = document.createElement('p'),
+        pProfileNoClass = document.createElement('p'), 
+        aChildNoClass = document.createElement('a'),
+        pFollowersNoClass = document.createElement('p'), 
+        pFollowingNoClass = document.createElement('p'), 
+        pBioNoClass = document.createElement('p'); 
 
-// Step 2: Inspect and study the data coming back, this is YOUR 
-//    github info! You will need to understand the structure of this 
-//    data in order to use it to build your component function 
+  let heroImgLink = ""; 
+
+   divCard.classList.add('card'); 
+   divCardInfo.classList.add('card-info'); 
+   h3Name.classList.add('name'); 
+   pUserName.classList.add('user-name'); 
+
+  //  imgHeroNoClass.add.attributes(`src= heroImgLink`); 
+   h3Name.textContent="TESTING"; 
+
+   divCard.append(imgHeroNoClass);
+   divCard.append(divCardInfo);
+   divCard.append(h3Name);
+   divCard.append(pUserName); 
+   divCard.append(pLocationNoClass);
+   divCard.append(pProfileNoClass); 
+   divCard.append(aChildNoClass); 
+   divCard.append(pFollowersNoClass); 
+   divCard.append(pFollowingNoClass); 
+   divCard.append(pBioNoClass); 
+  
+
+
+   const board = document.querySelector('.cards'); 
+   board.append(divCard); 
+
+  // *<div class="card">
+  // *  <img src={image url of user} />
+  // *  <div class="card-info">
+  // *    <h3 class="name">{users name}</h3>
+  // *    <p class="username">{users user name}</p>
+  // *    <p>Location: {users location}</p>
+  // *    <p>Profile:  
+  // *      <a href={address to users github page}>{address to users github page}</a>
+  // *   </p>
+  // *   <p>Followers: {users followers count}</p>
+  // *    <p>Following: {users following count}</p>
+  // *    <p>Bio: {users bio}</p>
+  //   </div>
+  // </div>
+
 
 
 
@@ -223,8 +294,7 @@ axios.get("https://api.github.com/users/web25Lucius")
 
 
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+
 
 *<div class="card">
 *  <img src={image url of user} />
