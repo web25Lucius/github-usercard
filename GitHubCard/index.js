66245979b6978f687ef171cc17eb1ca3dfc740b2
Mona B <web25Lucius@gmail.com>
2020-03-07 07:00:47 -0700

    starting project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,45 +3,45 @@
            https://api.github.com/users/<your name>
 */
 
-const followersArray = [];
-axios.get('https://api.github.com/users/web25Lucius')
-.then(response => { 
-  console.log(response.data); 
-  card.appendChild(myGit(response.data));
-  axios.get('response.data.followers_url')
-    .then(response => {
-      let followersArrays = response.data;
-      followersArrays.forEach( follower => followersArray.push(follower.login));
-    }) 
-    followersArray.forEach( item => {
-      axios.get('https://api.github.com/users/${item}')
-        .then( response => {
-          console.log(response.data);
-         card.appendChild(myGit(response.data));
-        })
-    })
-})
-.catch(error => {
-    console.log("ERROR .get did not work ERROR", error);
+// const followersArray = [];
+// axios.get('https://api.github.com/users/web25Lucius')
+// .then(response => { 
+//   console.log(response.data); 
+//   card.appendChild(myGit(response.data));
+//   axios.get('response.data.followers_url')
+//     .then(response => {
+//       let followersArrays = response.data;
+//       followersArrays.forEach( follower => followersArray.push(follower.login));
+//     }) 
+//     followersArray.forEach( item => {
+//       axios.get('https://api.github.com/users/${item}')
+//         .then( response => {
+//           console.log(response.data);
+//          card.appendChild(myGit(response.data));
+//         })
+//     })
+// })
+// .catch(error => {
+//     console.log("ERROR .get did not work ERROR", error);
   
-  }); 
+//   }); 
 
-  axios.get('https://api.github.com/users/web25Lucius/followers')
-  .then( response => {
-    // Handles Success
-    console.log(response);
-    response.data.forEach( element => {
-      followersArray.push(element);
-    })
-    followersArray.forEach( element => {
-      card.appendChild(myGit(element));
-    })
-  })
-  .catch( error => {
-    console.log(error);
-  })
+//   axios.get('https://api.github.com/users/web25Lucius/followers')
+//   .then( response => {
+//     // Handles Success
+//     console.log(response);
+//     response.data.forEach( element => {
+//       followersArray.push(element);
+//     })
+//     followersArray.forEach( element => {
+//       card.appendChild(myGit(element));
+//     })
+//   })
+//   .catch( error => {
+//     console.log(error);
+//   })
 
-  console.log(followersArray);
+//   console.log(followersArray);
   //response.data <so I don't have to keep typing data.data
 /* .then(response => {
     console.log(response);
@@ -76,99 +76,99 @@ Step 2: Inspect and study the data coming back, this is YOUR
 
 
 
-const card = document.querySelector('.cards'); 
+// const card = document.querySelector('.cards'); 
 
-function myGit (obj){
+// function myGit (obj){
  
-  const card = document.createElement('div');
-  const img = document.createElement('img');
-  const cardInfo = document.createElement('div');
-  const h3Name = document.createElement('h3');
-  const pUser = document.createElement('p');
-  const userLocation = document.createElement('p');
-  const profile = document.createElement('p');
-  const aProfile = document.createElement('a');
-  const follower = document.createElement('p');
-  const following = document.createElement('p');
-  const bio = document.createElement('p');
-  // Class add
-  card.classList.add('card');
-  cardInfo.classList.add('card-info');
-  h3Name.classList.add('name');
-  pUser.classList.add('username');
-  // Append Elements to cards
-  card.appendChild(img);
-  card.appendChild(cardInfo);
-  cardInfo.appendChild(h3Name);
-  cardInfo.appendChild(pUser);
-  cardInfo.appendChild(userLocation);
-  cardInfo.appendChild(profile);
-  cardInfo.appendChild(aProfile);
-  cardInfo.appendChild(follower);
-  cardInfo.appendChild(following);
-  cardInfo.appendChild(bio);
-  img.src = `${obj.avatar_url}`;
-  img.textContent = `${obj.image}`;
-  h3Name.textContent = `${obj.name}`;
-  if (obj.name === undefined) {
-    h3Name.remove();
-  }
-  pUser.textContent = `${obj.login}`;
-  userLocation.textContent = `${obj.location}`;
-  aProfile.textContent = `Profile: ${obj.html_url}`;
-  aProfile.href = `${obj.html_url}`;
-  follower.textContent = `Followers: ${obj.followers}`;
-  following.textContent = `Follow: ${obj.following}`;
-  bio.textContent = `${obj.bio}`;
-  return card;
+//   const card = document.createElement('div');
+//   const img = document.createElement('img');
+//   const cardInfo = document.createElement('div');
+//   const h3Name = document.createElement('h3');
+//   const pUser = document.createElement('p');
+//   const userLocation = document.createElement('p');
+//   const profile = document.createElement('p');
+//   const aProfile = document.createElement('a');
+//   const follower = document.createElement('p');
+//   const following = document.createElement('p');
+//   const bio = document.createElement('p');
+//   // Class add
+//   card.classList.add('card');
+//   cardInfo.classList.add('card-info');
+//   h3Name.classList.add('name');
+//   pUser.classList.add('username');
+//   // Append Elements to cards
+//   card.appendChild(img);
+//   card.appendChild(cardInfo);
+//   cardInfo.appendChild(h3Name);
+//   cardInfo.appendChild(pUser);
+//   cardInfo.appendChild(userLocation);
+//   cardInfo.appendChild(profile);
+//   cardInfo.appendChild(aProfile);
+//   cardInfo.appendChild(follower);
+//   cardInfo.appendChild(following);
+//   cardInfo.appendChild(bio);
+//   img.src = `${obj.avatar_url}`;
+//   img.textContent = `${obj.image}`;
+//   h3Name.textContent = `${obj.name}`;
+//   if (obj.name === undefined) {
+//     h3Name.remove();
+//   }
+//   pUser.textContent = `${obj.login}`;
+//   userLocation.textContent = `${obj.location}`;
+//   aProfile.textContent = `Profile: ${obj.html_url}`;
+//   aProfile.href = `${obj.html_url}`;
+//   follower.textContent = `Followers: ${obj.followers}`;
+//   following.textContent = `Follow: ${obj.following}`;
+//   bio.textContent = `${obj.bio}`;
+//   return card;
 
-  // const divCard = document.createElement= ('div');
-  // divCard.classList.add('card'); 
+//   // const divCard = document.createElement= ('div');
+//   // divCard.classList.add('card'); 
   
-  // const image = document.createElement=('img'); 
-  // divCard.appendChild(image); 
-  // image.setAtrribute=('src', object.avatar_url); //  {data.avatar_url} 
+//   // const image = document.createElement=('img'); 
+//   // divCard.appendChild(image); 
+//   // image.setAtrribute=('src', object.avatar_url); //  {data.avatar_url} 
   
-  // const cInfoCard = document.createElement('div'); 
-  // cInfoCard.classList.add('card-info'); 
-  // divCard.appendChild(cInfoCard); 
+//   // const cInfoCard = document.createElement('div'); 
+//   // cInfoCard.classList.add('card-info'); 
+//   // divCard.appendChild(cInfoCard); 
 
-  // const headerName = document.createElement('h3'); 
-  // headerName.classList.add('name'); 
-  // headerName.textContent=(object.name); // {data.name}
-  // divCard.appendChild(headerName); 
+//   // const headerName = document.createElement('h3'); 
+//   // headerName.classList.add('name'); 
+//   // headerName.textContent=(object.name); // {data.name}
+//   // divCard.appendChild(headerName); 
 
-  // const pUser = document.createElement('p'); 
-  // pUser.classList.add('userName');
-  // pUser.textContent=(object.login); //{data.login} 
-  // divCard.appendChild(pUser); 
+//   // const pUser = document.createElement('p'); 
+//   // pUser.classList.add('userName');
+//   // pUser.textContent=(object.login); //{data.login} 
+//   // divCard.appendChild(pUser); 
 
-  // const pLocation = document.createElement('p');
-  // pLocation.textContent=`Location: ${object.location}`; //{data.location}
-  // divCard.appendChild(pLocation);
+//   // const pLocation = document.createElement('p');
+//   // pLocation.textContent=`Location: ${object.location}`; //{data.location}
+//   // divCard.appendChild(pLocation);
 
-  // const pProfile = document.createElement('p'); 
-  // pProfile.textContent=('Profile: '); 
-  // divCard.appendChild(pProfile); 
+//   // const pProfile = document.createElement('p'); 
+//   // pProfile.textContent=('Profile: '); 
+//   // divCard.appendChild(pProfile); 
   
-  // const aTag = document.createElement('a'); 
-  // aTag.setAttribute='href', `${object.html_url}`; // {data.html_url}
-  // pProfile.appendChild(aTag); 
+//   // const aTag = document.createElement('a'); 
+//   // aTag.setAttribute='href', `${object.html_url}`; // {data.html_url}
+//   // pProfile.appendChild(aTag); 
 
-  // const pFollower = document.createElement('p');
-  // pFollower.textContent=`Followers: ${object.followers}`;//{data.followers} 
-  // divCard.appendChild(pFollower); 
+//   // const pFollower = document.createElement('p');
+//   // pFollower.textContent=`Followers: ${object.followers}`;//{data.followers} 
+//   // divCard.appendChild(pFollower); 
 
-  // const pFllwing  = document.createElement('p');
-  // pFllwing.textContent= `Following: ${object.following}`; //{data.following}
-  // divCard.appendChild(pFllwing); 
+//   // const pFllwing  = document.createElement('p');
+//   // pFllwing.textContent= `Following: ${object.following}`; //{data.following}
+//   // divCard.appendChild(pFllwing); 
 
-  // const pBio  = document.createElement('p');
-  // pBio.textContent=`Bio: ${object.bio}`; //{data.bio}
-  // divCard.appendChild(pBio); 
+//   // const pBio  = document.createElement('p');
+//   // pBio.textContent=`Bio: ${object.bio}`; //{data.bio}
+//   // divCard.appendChild(pBio); 
 
-  // return divCard; 
-}; 
+//   // return divCard; 
+// }; 
 
 
 
@@ -200,7 +200,7 @@ function myGit (obj){
   luishrd
   bigknell
 */
-followersArray.forEach(item =>{
-  let followerCards = myGit(item);
-  card.appendChild(followerCards);
-})
+// followersArray.forEach(item =>{
+//   let followerCards = myGit(item);
+//   card.appendChild(followerCards);
+// })
