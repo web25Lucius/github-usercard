86dee925d83e7e102879c8081f63ff5b67b86e9b
Mona B <web25Lucius@gmail.com>
2020-03-07 10:11:37 -0700

    for in Loop through axios data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -59,17 +59,11 @@
 
 
 
-axios.get("https://api.github.com/users/web25Lucius")
-  .then(response =>{
-    console.log(response); 
-  })
-  .catch(error =>{
-    console.log("the data did not return", error); 
-  })
 
 
 
 
+function  gitCard (object) {
   const divCard = document.createElement('div'), 
         imgHeroNoClass = document.createElement('img'), 
         divCardInfo = document.createElement('div'),
@@ -82,15 +76,22 @@ axios.get("https://api.github.com/users/web25Lucius")
         pFollowingNoClass = document.createElement('p'), 
         pBioNoClass = document.createElement('p'); 
 
-  let heroImgLink = ""; 
+  
 
    divCard.classList.add('card'); 
    divCardInfo.classList.add('card-info'); 
    h3Name.classList.add('name'); 
    pUserName.classList.add('user-name'); 
 
-  //  imgHeroNoClass.add.attributes(`src= heroImgLink`); 
-   h3Name.textContent="TESTING"; 
+   imgHeroNoClass.setAttribute(src= `${object.avatar_url}`); 
+   h3Name.textContent=object.name; 
+   pUserName.textContent=object.login; 
+   pLocationNoClass.textContent = object.location; 
+   pProfileNoClass.textContent = `Profile: ${object.html_url}`; 
+   aChildNoClass.setAttribute(src=`${object.html_url}`); 
+   pFollowersNoClass.textContent = `Followers: ${object.followers}`;  
+   pFollowingNoClass.textContent = `Following: ${object.following}`; 
+   pBioNoClass.textContent = `Bio: ${object.bio}`; 
 
    divCard.append(imgHeroNoClass);
    divCard.append(divCardInfo);
@@ -102,225 +103,30 @@ axios.get("https://api.github.com/users/web25Lucius")
    divCard.append(pFollowersNoClass); 
    divCard.append(pFollowingNoClass); 
    divCard.append(pBioNoClass); 
+
+
+   return divCard; 
   
+}
+const board = document.querySelector('.cards'); 
 
-
-   const board = document.querySelector('.cards'); 
-   board.append(divCard); 
-
-  // *<div class="card">
-  // *  <img src={image url of user} />
-  // *  <div class="card-info">
-  // *    <h3 class="name">{users name}</h3>
-  // *    <p class="username">{users user name}</p>
-  // *    <p>Location: {users location}</p>
-  // *    <p>Profile:  
-  // *      <a href={address to users github page}>{address to users github page}</a>
-  // *   </p>
-  // *   <p>Followers: {users followers count}</p>
-  // *    <p>Following: {users following count}</p>
-  // *    <p>Bio: {users bio}</p>
-  //   </div>
-  // </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const followersArray = [];
-// axios.get('https://api.github.com/users/web25Lucius')
-// .then(response => { 
-//   console.log(response.data); 
-//   card.appendChild(myGit(response.data));
-//   axios.get('response.data.followers_url')
-//     .then(response => {
-//       let followersArrays = response.data;
-//       followersArrays.forEach( follower => followersArray.push(follower.login));
-//     }) 
-//     followersArray.forEach( item => {
-//       axios.get('https://api.github.com/users/${item}')
-//         .then( response => {
-//           console.log(response.data);
-//          card.appendChild(myGit(response.data));
-//         })
-//     })
-// })
-// .catch(error => {
-//     console.log("ERROR .get did not work ERROR", error);
-  
-//   }); 
-
-//   axios.get('https://api.github.com/users/web25Lucius/followers')
-//   .then( response => {
-//     // Handles Success
-//     console.log(response);
-//     response.data.forEach( element => {
-//       followersArray.push(element);
-//     })
-//     followersArray.forEach( element => {
-//       card.appendChild(myGit(element));
-//     })
-//   })
-//   .catch( error => {
-//     console.log(error);
-//   })
-
-//   console.log(followersArray);
-  //response.data <so I don't have to keep typing data.data
-/* .then(response => {
-    console.log(response);
-    response.data.message.forEach(item => {
-      const newDog = DogCard(item);
-      entryPoint.appendChild(newDog);
-    });
+axios.get("https://api.github.com/users/web25Lucius")
+  .then(response => {
+  for (const item in response) {
+    const newGitCard = gitCard(item);
+       board.appendChild(newGitCard)
+  }
+      // response.data.forEach(item => {
+      //   const newGitCard = gitCard(item);
+      //     board.appendChild(newGitCard)
+//  });
+})
+  .catch( error => {
+    console.log("the data did not return", error);
   })
-  .catch(error => {
-    console.log("The data was not returned", error);
-  });
 
 
-
-
-// const card = document.querySelector('.cards'); 
-
-// function myGit (obj){
  
-//   const card = document.createElement('div');
-//   const img = document.createElement('img');
-//   const cardInfo = document.createElement('div');
-//   const h3Name = document.createElement('h3');
-//   const pUser = document.createElement('p');
-//   const userLocation = document.createElement('p');
-//   const profile = document.createElement('p');
-//   const aProfile = document.createElement('a');
-//   const follower = document.createElement('p');
-//   const following = document.createElement('p');
-//   const bio = document.createElement('p');
-//   // Class add
-//   card.classList.add('card');
-//   cardInfo.classList.add('card-info');
-//   h3Name.classList.add('name');
-//   pUser.classList.add('username');
-//   // Append Elements to cards
-//   card.appendChild(img);
-//   card.appendChild(cardInfo);
-//   cardInfo.appendChild(h3Name);
-//   cardInfo.appendChild(pUser);
-//   cardInfo.appendChild(userLocation);
-//   cardInfo.appendChild(profile);
-//   cardInfo.appendChild(aProfile);
-//   cardInfo.appendChild(follower);
-//   cardInfo.appendChild(following);
-//   cardInfo.appendChild(bio);
-//   img.src = `${obj.avatar_url}`;
-//   img.textContent = `${obj.image}`;
-//   h3Name.textContent = `${obj.name}`;
-//   if (obj.name === undefined) {
-//     h3Name.remove();
-//   }
-//   pUser.textContent = `${obj.login}`;
-//   userLocation.textContent = `${obj.location}`;
-//   aProfile.textContent = `Profile: ${obj.html_url}`;
-//   aProfile.href = `${obj.html_url}`;
-//   follower.textContent = `Followers: ${obj.followers}`;
-//   following.textContent = `Follow: ${obj.following}`;
-//   bio.textContent = `${obj.bio}`;
-//   return card;
-
-//   // const divCard = document.createElement= ('div');
-//   // divCard.classList.add('card'); 
-  
-//   // const image = document.createElement=('img'); 
-//   // divCard.appendChild(image); 
-//   // image.setAtrribute=('src', object.avatar_url); //  {data.avatar_url} 
-  
-//   // const cInfoCard = document.createElement('div'); 
-//   // cInfoCard.classList.add('card-info'); 
-//   // divCard.appendChild(cInfoCard); 
-
-//   // const headerName = document.createElement('h3'); 
-//   // headerName.classList.add('name'); 
-//   // headerName.textContent=(object.name); // {data.name}
-//   // divCard.appendChild(headerName); 
-
-//   // const pUser = document.createElement('p'); 
-//   // pUser.classList.add('userName');
-//   // pUser.textContent=(object.login); //{data.login} 
-//   // divCard.appendChild(pUser); 
-
-//   // const pLocation = document.createElement('p');
-//   // pLocation.textContent=`Location: ${object.location}`; //{data.location}
-//   // divCard.appendChild(pLocation);
-
-//   // const pProfile = document.createElement('p'); 
-//   // pProfile.textContent=('Profile: '); 
-//   // divCard.appendChild(pProfile); 
-  
-//   // const aTag = document.createElement('a'); 
-//   // aTag.setAttribute='href', `${object.html_url}`; // {data.html_url}
-//   // pProfile.appendChild(aTag); 
-
-//   // const pFollower = document.createElement('p');
-//   // pFollower.textContent=`Followers: ${object.followers}`;//{data.followers} 
-//   // divCard.appendChild(pFollower); 
-
-//   // const pFllwing  = document.createElement('p');
-//   // pFllwing.textContent= `Following: ${object.following}`; //{data.following}
-//   // divCard.appendChild(pFllwing); 
-
-//   // const pBio  = document.createElement('p');
-//   // pBio.textContent=`Bio: ${object.bio}`; //{data.bio}
-//   // divCard.appendChild(pBio); 
-
-//   // return divCard; 
-// }; 
 
 
 
-
-
-
-*<div class="card">
-*  <img src={image url of user} />
-*  <div class="card-info">
-*    <h3 class="name">{users name}</h3>
-*    <p class="username">{users user name}</p>
-*    <p>Location: {users location}</p>
-*    <p>Profile:  
-*      <a href={address to users github page}>{address to users github page}</a>
-*   </p>
-*   <p>Followers: {users followers count}</p>
-*    <p>Following: {users following count}</p>
-*    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
-// followersArray.forEach(item =>{
-//   let followerCards = myGit(item);
-//   card.appendChild(followerCards);
-// })
